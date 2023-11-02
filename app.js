c5c4e757cfbf6433e08a011342dda61af18d074d
Avi Mukesh <avimukesh10@gmail.com>
2023-11-02 22:14:15 +0000

    trynna fix cors

--- app.js
+++ app.js
@@ -20,11 +20,20 @@ app.use(
         ],
     })
 )
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(cookieParser())
 app.use(express.json())
 app.use("/auth", authRouter)
 
-require("dotenv").config()
+// require("dotenv").config()
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
